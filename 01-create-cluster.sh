89e293121d02dd4ebe4f88c0ece6ead8dd045f83
Theara Seng <seng.theara.kh@gmail.com>
2021-09-29 22:12:27 +0700

    2 nodes

--- 01-create-cluster.sh
+++ 01-create-cluster.sh
@@ -2,8 +2,9 @@ kops create cluster \
 --name=kubernetes.thearaseng.com \
 --state=s3://kops-state-34kkk-44dd3-dfjfd \
 --zones=ap-southeast-1a,ap-southeast-1b \
---node-count=1 \
+--node-count=2 \
 --node-size=t2.medium \
 --master-size=t2.medium \
 --dns-zone=kubernetes.thearaseng.com \
---cloud=aws
+--cloud=aws \
+--yes
